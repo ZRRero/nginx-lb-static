0cf592ac904138ef48951ae3b4709584d81d109e
David Useche <david-use@hotmail.com>
2023-05-15 22:06:11 -0500

    Fixes on launch template

--- ec2.tf
+++ ec2.tf
@@ -65,8 +65,12 @@ resource "aws_launch_template" "static_launch_template" {
   iam_instance_profile {
     arn = aws_iam_instance_profile.static_instance_profile.arn
   }
+  network_interfaces {
+    security_groups = [aws_security_group.static_security_group.id]
+    associate_public_ip_address = true
+    delete_on_termination =true
+  }
   user_data = filebase64("webconfig/user-data-static.sh")
-  vpc_security_group_ids = [aws_security_group.static_security_group.id]
   metadata_options {
     http_endpoint = "enabled"
     http_tokens = "required"
@@ -80,13 +84,32 @@ resource "aws_launch_template" "load_balancer_launch_template" {
   image_id = data.aws_ami.base_ami.image_id
   instance_type = var.load_balancer_instance_type
   iam_instance_profile {
-    arn = aws_iam_instance_profile.static_instance_profile.arn
+    arn = aws_iam_instance_profile.load_balancer_instance_profile.arn
+  }
+  network_interfaces {
+    security_groups = [aws_security_group.load_balancer_security_group.id]
+    associate_public_ip_address = true
+    delete_on_termination =true
   }
   user_data = filebase64("webconfig/user-data-lb.sh")
-  vpc_security_group_ids = [aws_security_group.load_balancer_security_group.id]
   metadata_options {
     http_endpoint = "enabled"
     http_tokens = "required"
     instance_metadata_tags = "enabled"
   }
+}
+
+resource "aws_instance" "static_instance" {
+  provider = aws.master_region
+  count = var.static_instance_count
+  launch_template {
+    id = aws_launch_template.static_launch_template.id
+    version = "$Latest"
+  }
+  tags = {
+    "Number": count.index
+    "Owner": "load_balancer"
+    "Bucket": aws_s3_bucket.configuration_bucket.bucket
+  }
+  subnet_id = aws_subnet.public_subnets[count.index % length(aws_subnet.public_subnets)].id
 }
