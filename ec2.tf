6671f970c46513233afb8f455429e7346dcf29e3
David Useche <david-use@hotmail.com>
2023-05-18 09:34:44 -0500

    Delete creation on tf for instances

--- ec2.tf
+++ ec2.tf
@@ -78,34 +78,3 @@ resource "aws_launch_template" "load_balancer_launch_template" {
     instance_metadata_tags = "enabled"
   }
 }
-
-resource "aws_instance" "static_instance" {
-  provider = aws.master_region
-  count = length(var.static_instances_weights)
-  launch_template {
-    id = aws_launch_template.static_launch_template.id
-    version = "$Latest"
-  }
-  tags = {
-    "Number": count.index
-    "Owner": "load_balancer"
-    "Bucket": aws_s3_bucket.configuration_bucket.bucket
-    "Name": "Static-${count.index}"
-    "Weight": var.static_instances_weights[count.index]
-  }
-  subnet_id = aws_subnet.public_subnets[count.index % length(aws_subnet.public_subnets)].id
-}
-
-resource "aws_instance" "load_balancer_instance" {
-  provider = aws.master_region
-  launch_template {
-    id = aws_launch_template.load_balancer_launch_template.id
-    version = "$Latest"
-  }
-  tags = {
-    "Bucket": aws_s3_bucket.configuration_bucket.bucket
-    "Name": "load_balancer"
-  }
-  subnet_id = aws_subnet.public_subnets[0].id
-  depends_on = [aws_instance.static_instance]
-}
